85d17502fe98a16ef7be660ade252e9acb885e62
zzy <1138154255@qq.com>
2023-02-26 16:21:11 +0800

    springmvc 框架修改为config类模式

---
+++ src/main/java/zzy/esvp/config/ServletContainersInitConfig.java
@@ -0,0 +1,24 @@
+package zzy.esvp.config;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+public class ServletContainersInitConfig extends AbstractDispatcherServletInitializer {
+    @Override
+    protected WebApplicationContext createServletApplicationContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(SpringMvcConfig.class);
+        return context;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected WebApplicationContext createRootApplicationContext() {
+        return null;
+    }
+}
---
+++ src/main/java/zzy/esvp/config/SpringMvcConfig.java
@@ -0,0 +1,9 @@
+package zzy.esvp.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("zzy.esvp.controller")
+public class SpringMvcConfig {
+}
